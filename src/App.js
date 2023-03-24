15c75988c5dfaa160bf753f2665e3b7028b664a4
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2023-03-24 15:50:59 +0100

    start css with sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 const App = () => {
-  return <div>
-    <h1>test</h1>
-  </div>;
+  return (
+    <div>
+      <h1>React Cook</h1>
+      <input
+        type="text"
+        id="searchBar"
+        placeholder="Taper le nom d'un aliment (en anglais)"
+      />
+    </div>
+  );
 };
 
 export default App;
